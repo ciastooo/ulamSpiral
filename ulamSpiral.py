ddf9fb00afe163f21e3a74ca2c4e7300fb4b9445
Ciastooo <leene4@gmail.com>
2018-07-09 16:14:36 +0200

    Creating empty image file

--- ulamSpiral.py
+++ ulamSpiral.py
@@ -1,2 +1,13 @@
-msg = "init"
-print(msg)
+from PIL import Image
+
+size = input("Enter size of image:")
+while 1:
+    try:
+        size = int(size)
+        break
+    except ValueError:
+        print("Entered size is not a number")
+        size = input("Enter size of image:")
+
+img = Image.new('RGB', (size, size))
+img.save('image.png')
